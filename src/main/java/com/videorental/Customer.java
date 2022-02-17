a383d1e1e00eca37f1aa9ee470417439cade4853
Donghee Han <handh0413@naver.com>
2022-02-17 21:19:09 +0900

    코드 리팩토링(Extract Method)

--- src/main/java/com/videorental/Customer.java
+++ src/main/java/com/videorental/Customer.java
@@ -21,21 +21,22 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRentalPoints = 0;
         Iterator<Rental> iterator = rentals.iterator();
         String result = "Rental Record for " + getName() + "\n";
 
         while (iterator.hasNext()) {
             Rental each = (Rental) iterator.next();
-            // determine amounts for each line
-
-            // add frequent rental points
-            frequentRentalPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRentalPoints++;
             // show figures
             result += "\t" + String.valueOf(each.getCharge()) + "(" + each.getMovie().getTitle() + ")" + "\n";
+        }
+
+        int frequentRentalPoints = 0;
+        for (Rental rental : rentals) {
+            frequentRentalPoints++;
+            // add bonus for a two day new release rental
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) {
+                frequentRentalPoints++;
+            }
         }
 
         result += "Amount owed is " + getTotalAmount() + "\n";
