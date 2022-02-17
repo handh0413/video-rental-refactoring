b071b7ce14fb31b528059c8582554e14b8e55bdc
Donghee Han <handh0413@naver.com>
2022-02-17 21:37:14 +0900

    코드 리팩토링
    - extract method

--- src/main/java/com/videorental/Rental.java
+++ src/main/java/com/videorental/Rental.java
@@ -20,22 +20,26 @@ public class Rental {
     // ctrl + alt + shift + t > move instance method
     // shift + f6 > method 명 수정
     public double getCharge() {
+        return getChargeFor(daysRented);
+    }
+
+    private double getChargeFor(int daysRented) {
         double thisAmount = 0;
         switch (getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (getDaysRented() > 2)
-                    thisAmount += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
                 break;
 
             case Movie.NEW_RELEASE:
-                thisAmount += (getDaysRented()) * 3;
+                thisAmount += daysRented * 3;
                 break;
 
             case Movie.CHILDREN:
                 thisAmount += 1.5;
-                if (getDaysRented() > 3)
-                    thisAmount += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
                 break;
         }
         return thisAmount;
